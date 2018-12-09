da565d2391cb82932624247d5f00d3a179f65589
Kristiina <kristiina@hooligan.ee>
2018-12-09 22:22:06 +0200

    Update outdetect.R

--- R/outdetect.R
+++ R/outdetect.R
@@ -1,31 +1,35 @@
 extractOutliers <- function( data, minPoints, minCoher=0.7, k=2, rejCrit=3, minJacc = 0.6 ){
-  parameters <- list(minPoints=minPoints, minCoher=minCoher, k=k, rejCrit=rejCrit, minJacc=minJacc)
+  params <- list(minPoints=minPoints, minCoher=minCoher, k=k, rejCrit=rejCrit, minJacc=minJacc)
+  origColumnNames <- colnames(data)
+  params$variables <- origColumnNames[4:length(colnames(data))]
+  #validations
 
   allData <- prepareData(data)
 
   #Location based
-  dbscan <- getDbscan( allData, parameters )
-  parameters$eps <- dbscan$eps
+  dbscan <- getDbscan( allData, params )
+  params$eps <- dbscan$eps
   groupCluster <- as.integer(names(which.max(table(dbscan$cluster))))
   dataWithCluster <- bindClusterToData(dbscan, allData)
 
   #Data-driven
-  pointsWithOCFlag <- applyPCA( dataWithCluster, parameters ) #points with oc flag; k no of components
+  pointsWithOCFlag <- applyPCA( dataWithCluster, params ) #points with oc flag; k no of components
   outlierAndNoiseFree <- getOCFreeData( pointsWithOCFlag ) #points to be included in final dataset and found by PCA
-  noiseToBeIncluded <- getNoiseWithCoherLimit(pointsWithOCFlag, parameters)
+  noiseToBeIncluded <- getNoiseWithCoherLimit(pointsWithOCFlag, params)
 
   #Application-driven
-  madOfVariablePerClusterDF <- calculateMadOfVariablePerCluster( pointsWithOCFlag, dbscan, parameters )
-  additionalPoints <- processOCs( pointsWithOCFlag, madOfVariablePerClusterDF, parameters )
+  madOfVariablePerClusterDF <- calculateMadOfVariablePerCluster( pointsWithOCFlag, dbscan, params )
+  additionalPoints <- processOCs( pointsWithOCFlag, madOfVariablePerClusterDF, params )
   pointsToKeep <- rbind(outlierAndNoiseFree, noiseToBeIncluded, additionalPoints)
 
   nonoutliers <- data[data$ID %in% row.names(pointsToKeep),]
   outliers <- data[!data$ID %in% row.names(pointsToKeep),]
 
-  x <- list(nonoutliers = nonoutliers, outliers = outliers,parameters = parameters)
+  x <- list(nonoutliers = nonoutliers, outliers = outliers,params = params)
 
   return (x)
 }
+
 
 #TODO column + data validations
 prepareData <- function( data ){
@@ -33,14 +37,14 @@ prepareData <- function( data ){
   row.names(inData) <- data$ID
   xyCoords <- inData[,c("LON","LAT")]
   sp.dat <- sp::SpatialPointsDataFrame(xyCoords, inData, proj4string = sp::CRS("+proj=longlat +datum=WGS84"))
-  sp.dat <- sp::spTransform(sp.dat, sp::CRS("+init=epsg:3857"))
+  sp.dat <- sp::spTransform(sp.dat, sp::CRS("+init=epsg:3395"))
   return (as.data.frame(sp.dat))
 }
 
-getDbscan <- function( data, parameters ){
-  allCoords <- dplyr::select(data, LON.1, LAT.1)
-  eps <- calculateEps(allCoords,parameters$minPoints)
-  return (dbscan::dbscan(allCoords, eps, parameters$minPoints))
+getDbscan <- function( data, params ){
+  allCoords <- data[,c("LON.1", "LAT.1")]
+  eps <- calculateEps(allCoords,params$minPoints)
+  return (dbscan::dbscan(allCoords, eps, params$minPoints))
 }
 
 calculateEps <- function( allCoords, minPoints ){
@@ -59,11 +63,11 @@ bindClusterToData <- function( dbscan, data ){
 }
 
 #apply PCA, return original data + flgagAll true/false
-applyPCA <- function( data, parameters ){
-#TODO make columns dynamic
-  variables <- dplyr::select(data,HEIGHT,HEIGHT.WRT.DEM,SIGMA.HEIGHT,VEL,SIGMA.VEL,CUM.DISP,COHER)
+applyPCA <- function( data, params ){
+  variables <- data[,params$variables]
+
   varMatrix <- as.matrix(variables)
-  aR = rrcov::PcaHubert(x=varMatrix, k=parameters$k, mcd=FALSE)
+  aR = rrcov::PcaHubert(x=varMatrix, k=params$k, mcd=FALSE)
   isCore = aR@flag
   return(cbind(data, isCore, deparse.level = 1))
 }
@@ -75,8 +79,8 @@ getOCFreeData <- function( data ){
 removeNoisePoints <- function( data ){
   return (data[data$isCore == FALSE & data$cluster != 0,])
 }
-getNoiseWithCoherLimit <- function( data, parameters ){
-  return ( data[data$isCore == FALSE & data$cluster == 0 & data$COHER>parameters$minCoher,] )
+getNoiseWithCoherLimit <- function( data, params ){
+  return ( data[data$isCore == FALSE & data$cluster == 0 & data$COHER>params$minCoher,] )
 }
 
 #Application-driven outliers
@@ -86,26 +90,26 @@ getNoiseWithCoherLimit <- function( data, parameters ){
 #minCoher: default 0.7
 #rejCrit:
 #minJacc: index threshold default 0.6
-processOCs <- function( pointsWithOCFlag, madOfVariablesPerClusterDF, parameters ){
+processOCs <- function( pointsWithOCFlag, madOfVariablesPerClusterDF, params ){
   noiseFreeOutlierCandidates <- removeNoisePoints(pointsWithOCFlag)
-  triang <- applyTriangulation( noiseFreeOutlierCandidates, parameters )
-  delsgsOfGroupedOC <- triang$delsgs[triang$delsgs$dist<=parameters$eps,]
-  groupedOCstoKeep <- processGroupedOutliers( noiseFreeOutlierCandidates, delsgsOfGroupedOC, madOfVariablesPerClusterDF, parameters )
+  triang <- applyTriangulation( noiseFreeOutlierCandidates, params )
+  delsgsOfGroupedOC <- triang$delsgs[triang$delsgs$dist<=params$eps,]
+  groupedOCstoKeep <- processGroupedOutliers( noiseFreeOutlierCandidates, delsgsOfGroupedOC, madOfVariablesPerClusterDF, params )
 
   indsWithneighbors <- getIndsWithNeighbors(delsgsOfGroupedOC)
 
   indsOfIsolatedOutliers <- getIndsOfIsolatedOutliers( triang$ind.orig, indsWithneighbors ) #process isolated outliers
 
-  isolatedOCstoKeep <- processIsolatedOutliers( noiseFreeOutlierCandidates, indsOfIsolatedOutliers, madOfVariablesPerClusterDF, parameters )
+  isolatedOCstoKeep <- processIsolatedOutliers( noiseFreeOutlierCandidates, indsOfIsolatedOutliers, madOfVariablesPerClusterDF, params )
   return (rbind(groupedOCstoKeep,isolatedOCstoKeep))
   }
 
-processIsolatedOutliers <- function( noiseFreeOutlierCandidates, indsOfIsolatedOutliers, madOfVariablesPerClusterDF, parameters ){
+processIsolatedOutliers <- function( noiseFreeOutlierCandidates, indsOfIsolatedOutliers, madOfVariablesPerClusterDF, params ){
   idsToKeep <- vector()
   for (i in indsOfIsolatedOutliers){
     ocData <- noiseFreeOutlierCandidates[i,]
-    if ( ocData$COHER>parameters$minCoher ){
-      rejectionTable <- calculateMatrixPerOc(noiseFreeOutlierCandidates, i, madOfVariablesPerClusterDF)
+    if ( ocData$COHER>params$minCoher ){
+      rejectionTable <- calculateMatrixPerOc(noiseFreeOutlierCandidates, i, madOfVariablesPerClusterDF, params)
       if(rowSums(rejectionTable) < 2){
         idsToKeep <- c(idsToKeep,i)
       }
@@ -117,7 +121,7 @@ processIsolatedOutliers <- function( noiseFreeOutlierCandidates, indsOfIsolatedO
 #####TODO lon1,lat1
 #returns df with neighboring points and their distance
 #TODO errors
-applyTriangulation <- function( data, parameters ){
+applyTriangulation <- function( data, params ){
   # Voronoi
   triang <- deldir::deldir(data$LON.1, data$LAT.1 , digits=18) #aka triangulation
   triang$delsgs$dist <- raster::pointDistance(cbind(triang$delsgs$x1, triang$delsgs$y1),cbind(triang$delsgs$x2, triang$delsgs$y2), lonlat=FALSE)
@@ -137,9 +141,9 @@ getIndsOfIsolatedOutliers <- function( indOrig, indsWithneighbors ){
   return ( indOrig[!(indOrig %in% indsWithneighbors)] )
 }
 
-processGroupedOutliers <- function( noiseFreeOutlierCandidates, delsgsOfGroupedOC, madOfVariablesPerClusterDF, parameters ){
+processGroupedOutliers <- function( noiseFreeOutlierCandidates, delsgsOfGroupedOC, madOfVariablesPerClusterDF, params ){
   groupedComponents <- groupOCfromPCA( delsgsOfGroupedOC )
-  return ( processEachGroup( noiseFreeOutlierCandidates, groupedComponents, madOfVariablesPerClusterDF, parameters ) )
+  return ( processEachGroup( noiseFreeOutlierCandidates, groupedComponents, madOfVariablesPerClusterDF, params ) )
 }
 
 #Makes graph of nonisolated OCs and groupes them. Returns groups with indices of original data.
@@ -150,35 +154,33 @@ groupOCfromPCA <- function( delsgsOfGroupedOC ){ #ocFromPcaWihtoutNoise
   return ( igraph::groups( igraph::components( g ) ) )
 }
 
-processEachGroup <- function( noiseFreeOutlierCandidates, groupedComponents, madOfVariablesPerClusterDF, parameters ){
+processEachGroup <- function( noiseFreeOutlierCandidates, groupedComponents, madOfVariablesPerClusterDF, params ){
   pointsToKeep <- data.frame()
   for (i in 1:length(groupedComponents)){
-    rejtable <-calculateMatrixPerOc(noiseFreeOutlierCandidates, groupedComponents[[i]], madOfVariablesPerClusterDF )
-    pointsToKeep <- rbind(pointsToKeep,calculateJaccard(rejtable, noiseFreeOutlierCandidates, parameters))
+    rejtable <-calculateMatrixPerOc(noiseFreeOutlierCandidates, groupedComponents[[i]], madOfVariablesPerClusterDF, params )
+    pointsToKeep <- rbind(pointsToKeep,calculateJaccard(rejtable, noiseFreeOutlierCandidates, params))
   }
   return(pointsToKeep)
 }
 
 
-calculateMadOfVariablePerCluster <- function( pointsWithOCFlag, dbscan, parameters ){
+calculateMadOfVariablePerCluster <- function( pointsWithOCFlag, dbscan, params ){
   clustersWithMajOutliers <- getClustersWithMajorityOutliers( pointsWithOCFlag )
   groupCluster <- as.integer(names(which.max(table(dbscan$cluster))))
   cluster <- groupCluster
   clusters <-  as.integer(names(table(dbscan$cluster)))
-
-  groupClusterData <- pointsWithOCFlag[pointsWithOCFlag$cluster == groupCluster & pointsWithOCFlag$isCore == TRUE,
-                                       c("HEIGHT","HEIGHT.WRT.DEM","SIGMA.HEIGHT","VEL","SIGMA.VEL","CUM.DISP","COHER")]
-  madPerColumnForGroupCluster <- as.data.frame(lapply(groupClusterData, madPerColumnFun, rj=parameters$rejCrit))
+  groupClusterData <- pointsWithOCFlag[pointsWithOCFlag$cluster == groupCluster & pointsWithOCFlag$isCore == TRUE,params$variables]
+  madPerColumnForGroupCluster <- as.data.frame(lapply(groupClusterData, madPerColumnFun, rj=params$rejCrit))
   result <- cbind(cluster, madPerColumnForGroupCluster )
-  for (i in clusters[-1]){ #TODO selle saaks ilusamaks
+  for (i in clusters[-1]){ 
     if ( i != groupCluster){
       cluster <- i
       if( cluster %in% clustersWithMajOutliers ){
         result <- rbind(result,cbind(cluster,madPerColumnForGroupCluster))
       } else {
         allNonOutliers <-  pointsWithOCFlag[pointsWithOCFlag$cluster == cluster & pointsWithOCFlag$isCore == TRUE,]
-        allNonOutliers1 <-  allNonOutliers[,c("HEIGHT","HEIGHT.WRT.DEM","SIGMA.HEIGHT","VEL","SIGMA.VEL","CUM.DISP","COHER")]
-        madPerColumn <-  as.data.frame(lapply(allNonOutliers1, madPerColumnFun, rj=parameters$rejCrit))
+        allNonOutliers1 <-  allNonOutliers[,params$variables]
+        madPerColumn <-  as.data.frame(lapply(allNonOutliers1, madPerColumnFun, rj=params$rejCrit))
         result <- rbind(result,cbind(cluster,madPerColumn))
       }
     }
@@ -187,7 +189,7 @@ calculateMadOfVariablePerCluster <- function( pointsWithOCFlag, dbscan, paramete
 }
 
 
-calculateJaccard <- function(rejList, allData, parameters){
+calculateJaccard <- function(rejList, allData, params){
   ocGroupData <- allData[row.names(allData) %in% row.names(rejList),]
   jacc <- dist(rejList, method = "binary",
                diag = TRUE, upper = TRUE)
@@ -198,11 +200,11 @@ calculateJaccard <- function(rejList, allData, parameters){
   pairWiseJacc$id <- factor(pairWiseJacc$id, rownames(jaccsim))
   pairWiseJacc$variable <- factor(pairWiseJacc$variable, rev(rownames(jaccsim)))
   pairWiseJacc <- plyr::arrange(pairWiseJacc, variable, plyr::desc(id))
-  different <- pairWiseJacc[pairWiseJacc$value < parameters$minJacc,]
+  different <- pairWiseJacc[pairWiseJacc$value < params$minJacc,]
   if (length(different[,1]) == 0){
     return(ocGroupData)
   }
-  return(ocGroupData[row.names(ocGroupData) %in% unique(different$id) & ocGroupData$COHER> parameters$minCoher,])
+  return(ocGroupData[row.names(ocGroupData) %in% unique(different$id) & ocGroupData$COHER> params$minCoher,])
 }
 
 getClustersWithMajorityOutliers <- function(data){
@@ -223,14 +225,13 @@ getClustersWithMajorityOutliers <- function(data){
   return( e[e$falseSum > e$trueSum,]$cluster )
 }
 
-calculateMatrixPerOc <- function(noiseFreeOutlierCandidates, ocInds, madOfVariablesPerClusterDF ){ #viga, kui clusterid ei ole samad
+calculateMatrixPerOc <- function(noiseFreeOutlierCandidates, ocInds, madOfVariablesPerClusterDF, params ){ #viga, kui clusterid ei ole samad
   ocInds <- as.integer(ocInds)
   cluster <-  noiseFreeOutlierCandidates[ocInds[1],]$cluster
-
   madValues <- madOfVariablesPerClusterDF[madOfVariablesPerClusterDF$cluster == cluster, -1]
 
-  rejectionTable <- noiseFreeOutlierCandidates[ocInds,c("HEIGHT","HEIGHT.WRT.DEM","SIGMA.HEIGHT","VEL","SIGMA.VEL","CUM.DISP","COHER")]
-  for(i in 1:7){ #selle seitsme saab eraldi muutujaks length(väljad) vms
+  rejectionTable <- noiseFreeOutlierCandidates[ocInds,params$variables]
+  for(i in 1:length(params$variables)){
     madPerColumn <-
       rejectionTable[,i]<-ifelse(dplyr::between(rejectionTable[,i], madValues[1,i], madValues[2,i]),0,1)
   }
